cd648254405d369011dc369aaaa4e3432559645c
Ody <melodymonyok@gmail.com>
2021-06-18 20:11:46 -0500

    Updated some file structure. Command line questions for employee data are added to index.js.

---
+++ index.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const html = require('./src/html');
+const fs = require('fs');
+const util = require('util');
+
+let employeeData = [];
+
+// const writeFileAsync = util.promisify(fs.writeFile);
+const questions = [
+    {
+        message: "What is your employee's full Name?",
+        name: 'name',
+    },
+    {
+        type: 'number',
+        message: "What is your employee's ID number?",
+        name: 'id',
+    },
+    {
+        message: "What is your employee's email address?",
+        name: 'email',
+    },
+    {
+        type: 'list',
+        message: "What is the role of your employee?",
+        choices: ['Manager', 'Engineer', 'Intern'],
+        name: 'role',
+    },
+    {
+        type: 'number',
+        message: "What is your manager's office number?",
+        name: 'office',
+        when(data) {
+            return data.role === 'Manager';
+        },
+    },
+    {
+        message: "What is your engineer's GitHub username?",
+        name: 'github',
+        when(data) {
+            return data.role === 'Engineer';
+        },
+    },
+    {
+        message: "What is the name of your intern's school?",
+        name: 'school',
+        when(data) {
+            return data.role === 'Intern';
+        },
+    },
+    {
+        type: 'confirm',
+        message: "Do you want to add another employee?",
+        name: 'askAgain',
+    },
+];
+
+function askQuestions() {
+    inquirer.prompt(questions).then((answers) => {
+        employeeData.push(answers);
+        if (answers.askAgain) {
+            askQuestions();
+        } else {
+            console.log("Questions are done!");
+            console.log(employeeData);
+        }
+    })
+}
+
+askQuestions();
